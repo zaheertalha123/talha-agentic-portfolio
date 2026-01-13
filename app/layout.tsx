cfb286701caa4e5c3c52ad464634b643124c2a86
Talha <zaheertalha123@gmail.com>
2026-01-13 05:58:29 -0800

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 // import { ScrollProgressIndicator } from "@/components/scroll-progress-indicator"
 import { AnimationProvider } from "@/contexts/animation-context";
 import { getMetaInfo } from "@/lib/data";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         <AnimationProvider>
           {/* <ScrollProgressIndicator /> */}
           {children}
+          <Analytics/>
         </AnimationProvider>
       </body>
     </html>
